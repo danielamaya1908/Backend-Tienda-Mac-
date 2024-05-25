4724cef392a41b91aa18f5038038d5ed9ae6b684
Daniel Amaya <danijcdm.com@gmail.com>
2024-05-25 07:36:17 -0500

    backend

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/images', express.static('C:\\Users\\USER\\OneDrive\\Escritorio\\Tienda-Mac\\ImagesProducts'));
+app.use('/images', express.static('C:\\Users\\USER\\OneDrive\\Escritorio\\ImagesProducts'));
 
 const upload = multer({ dest: "uploads/" });
 
