4d1ed7952e8e2dcad83e9d6e4dcd633652d4957a
Daniel Amaya <danijcd.com@gmail.com>
2024-09-27 02:33:52 -0500

    123

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -123,8 +123,8 @@ const getProperty = require("../controllers/product/getProperty");
 
 const getDiscountProducts = require("../controllers/product/getDiscountProducts");
 /* const getUserByEmail = require("../controllers/User/getUserByEmail"); */
-/* const postFavorite = require("../controllers/User/postFavorite");
-const deleteFavorite = require("../controllers/User/deleteFavorite"); */
+const postFavorite = require("../controllers/User/postFavorite");
+const deleteFavorite = require("../controllers/User/deleteFavorite");
 const getAllFavorites = require("../controllers/User/getAllFavorites");
 const getFavsByProduct = require("../controllers/User/getAllFavsByProduct");
 const setFavoritesByUser = require("../controllers/User/getFavoritesByUser");
