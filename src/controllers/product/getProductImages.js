bf681a391446559037b097c0a483dd8f43adfaa9
Daniel Amaya <danijcd.com@gmail.com>
2024-11-02 16:35:39 -0500

    conPath

--- src/controllers/product/getProductImages.js
+++ src/controllers/product/getProductImages.js
@@ -1,23 +1,20 @@
-const { Image } = require("../../db");
+const { ImageProduct } = require("../../db");
 
 const getProductImages = async (req, res) => {
   try {
     const { productId } = req.params;
 
-    const images = await Image.findAll({
+    const images = await ImageProduct.findAll({
       where: { productId },
-      attributes: ['id', 'path'],
+      attributes: ['id', 'path'], // Incluimos 'path' para obtener la ruta de las imágenes
     });
 
     if (!images || images.length === 0) {
       return res.status(404).json({ message: "No images found for the product" });
     }
 
-    const imagePaths = images.map(image => {
-      // Obtener solo el nombre del archivo de la ruta completa
-      const fileName = image.path.split('/').pop();
-      return fileName;
-    });
+    // Devolvemos las rutas de las imágenes
+    const imagePaths = images.map(image => image.path);
 
     res.status(200).json(imagePaths);
   } catch (error) {
