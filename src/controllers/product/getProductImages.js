7a0feaaa047e9fb056c78990dcaecf58f07babb1
Daniel Amaya <danijcd.com@gmail.com>
2024-11-03 00:43:57 -0500

    deletejpg

--- src/controllers/product/getProductImages.js
+++ src/controllers/product/getProductImages.js
@@ -1,25 +1,25 @@
-const { Image } = require("../../db");
+const { ImageProduct } = require("../../db");
 
 const getProductImages = async (req, res) => {
   try {
     const { productId } = req.params;
 
-    const images = await Image.findAll({
+    const images = await ImageProduct.findAll({
       where: { productId },
-      attributes: ['id', 'path'],
+      attributes: ['id', 'imageData'], // Asegúrate de incluir 'imageData'
     });
 
     if (!images || images.length === 0) {
       return res.status(404).json({ message: "No images found for the product" });
     }
 
-    const imagePaths = images.map(image => {
-      // Obtener solo el nombre del archivo de la ruta completa
-      const fileName = image.path.split('/').pop();
-      return fileName;
-    });
+    // Convertir los datos binarios a base64
+    const base64Images = images.map(image => ({
+      id: image.id,
+      data: image.imageData.toString('base64'), // Convertimos a base64
+    }));
 
-    res.status(200).json(imagePaths);
+    res.status(200).json(base64Images);
   } catch (error) {
     console.error("Error getting product images:", error);
     res.status(500).json({ message: "Error getting product images" });
