c0b486a84d9ac3d095b20886b53398a1c3ab4c0b
Daniel Amaya <danijcd.com@gmail.com>
2024-11-03 00:39:20 -0500

    deletejpg

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ const updateProductQuantity = require('./controllers/product/updateProductQuanti
 const soporteTecnicoRoutes = require('./routes/soporteTecnico.routes');
 
 // Importar la función de migración
-/* const migrateImages = require('./scripts/migrateImages'); // Asegúrate de que la ruta sea correcta */
+const migrateImages = require('./scripts/migrateImages'); // Asegúrate de que la ruta sea correcta
+
+
 
 const app = express();
 const router = express.Router(); // Inicializa el router
@@ -27,8 +29,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 
 // Ejecutar la migración al iniciar la aplicación
-/* migrateImages(); // Llama a la función de migración */
-
+migrateImages(); // Llama a la función de migración
 
 // Usa el router para definir la ruta
 router.post('/update-quantity', updateProductQuantity);
