c8b1ba87c14c49b1615653978a3a92395b5da816
Daniel Amaya <danijcdm.com@gmail.com>
2024-12-21 22:33:12 -0500

    cosa

--- src/models/SoporteTecnico.js
+++ src/models/SoporteTecnico.js
@@ -110,6 +110,10 @@ module.exports = (sequelize) => {
       type: DataTypes.TEXT,
       allowNull: true,
     },
+    claveEquipo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
 
   return SoporteTecnico;
